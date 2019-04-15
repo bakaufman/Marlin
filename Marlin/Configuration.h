9a2b1d8a8bfad9c4fb8d916ab7b4e689bc46219c
InsanityAutomation <38436470+InsanityAutomation@users.noreply.github.com>
2019-04-15 17:33:11 -0400

    [1.1.x] BLTOUCH_V3 parity with 2.0.x (#13682)
    Co-Authored-By: InsanityAutomation <insanityautomation@users.noreply.github.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -730,7 +730,17 @@
 //#define BLTOUCH
 #if ENABLED(BLTOUCH)
   //#define BLTOUCH_DELAY 375   // (ms) Enable and increase if needed
+  
+  /**
+   * BLTouch V3.0 and newer smart series
+   * For genuine BLTouch 3.0 sensors. Clones may be confused by 3.0 command angles. YMMV.
+   * If the pin trigger is not detected, first try swapping the black and white wires then toggle this.
+   */
   //#define BLTOUCH_V3
+  #if ENABLED(BLTOUCH_V3)
+    //#define BLTOUCH_FORCE_5V_MODE
+    //#define BLTOUCH_FORCE_OPEN_DRAIN_MODE
+  #endif
 #endif
 //#define PROBING_FANS_OFF          // Turn fans off when probing
 //#define DELAY_BEFORE_PROBING 200  // (ms) To prevent vibrations from triggering piezo sensors

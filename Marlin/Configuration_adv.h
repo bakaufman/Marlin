cf68fe2ecdaf2d474eede814361591e51ef6fbaa
Brett <31897832+bakaufman@users.noreply.github.com>
2020-07-26 22:14:43 -0400

    20200726 railcore update
    bugfix for sure

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2360,7 +2360,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      860       // BAK (1250/1.414). 680, 860 
+    #define E0_CURRENT      800       // BAK (1250/1.414). 680, 800, 860 
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1

fad75df0c94c48c2e4ce336593bf51a79f231fdd
Brett <31897832+bakaufman@users.noreply.github.com>
2020-08-12 08:19:09 -0400

    compiling for E3

--- Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_common.h
+++ Marlin/src/pins/stm32f1/pins_BTT_SKR_MINI_E3_common.h
@@ -48,12 +48,12 @@
 //
 #define X_STOP_PIN                          PC0   // "X-STOP"
 #define Y_STOP_PIN                          PC1   // "Y-STOP"
-#define Z_STOP_PIN                          PC2   // "Z-STOP"
+#define Z_STOP_PIN                          PC14   // "Z-STOP" BAK
 
 //
 // Z Probe must be this pin
 //
-#define Z_MIN_PROBE_PIN                     PC14  // "PROBE"
+#define Z_MIN_PROBE_PIN                     PC2  // "PROBE" BAK
 
 //
 // Filament Runout Sensor

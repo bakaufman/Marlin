fdcf628a3958b1128529cceab7b412a2cabdb364
Brett <31897832+bakaufman@users.noreply.github.com>
2020-12-22 22:20:27 -0500

    Update Configuration.h
    added back junction deviation because it looked awful at zero. TT doesn't do it with zero

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -826,7 +826,7 @@
  *   https://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM 0.0 // (mm) Distance from real junction edge 0.013
+  #define JUNCTION_DEVIATION_MM 0.013 // (mm) Distance from real junction edge 0.013
   #define JD_HANDLE_SMALL_SEGMENTS    // Use curvature estimation instead of just the junction angle
                                       // for small segments (< 1mm) with large junction angles (> 135°).
 #endif

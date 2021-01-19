b2b5a8b1ee3a3f11d783434a5997630c2791f2b5
Brett <31897832+bakaufman@users.noreply.github.com>
2021-01-19 12:51:37 -0500

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2494,7 +2494,7 @@
    * The default SW SPI pins are defined the respective pins files,
    * but you can override or define them here.
    */
-  #define TMC_USE_SW_SPI//BAK
+  //#define TMC_USE_SW_SPI//BAK 20210129
   //#define TMC_SW_MOSI       -1
   //#define TMC_SW_MISO       -1
   //#define TMC_SW_SCK        -1
